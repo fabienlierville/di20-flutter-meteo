0134ba6a0825efc0199b87d3a206c8720138ed9f
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-06 09:03:40 +0100

    Location

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 import 'package:meteo/pages/page_home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Location location = Location();
+
+  bool _serviceEnabled = await location.serviceEnabled();
+  if(!_serviceEnabled){
+    _serviceEnabled = await location.requestService();
+    if(!_serviceEnabled){
+      return;
+    }
+  }
+
+  PermissionStatus _persmissionGranted = await location.hasPermission();
+  if(_persmissionGranted == PermissionStatus.denied){
+    _persmissionGranted = await location.requestPermission();
+    if(_persmissionGranted != PermissionStatus.granted){
+      return;
+    }
+  }
+
   runApp(const MyApp());
 }
 
